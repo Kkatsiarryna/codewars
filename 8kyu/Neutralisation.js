07d76add01253484d3bab105a5173ad8f5359d14
Kkatsiarryna <ek4terina.98@yandex.by>
2024-01-25 01:20:22 +0300

    added new codewars challenge

---
+++ 8kyu/Neutralisation.js
@@ -0,0 +1,16 @@
+//Given two strings comprised of + and -, return a new string which shows how the two strings interact in the following way:
+
+//When positives and positives interact, they remain positive.
+//When negatives and negatives interact, they remain negative.
+//But when negatives and positives interact, they become neutral, and are shown as the number 0.
+
+//My solution
+function neutralise(s1, s2) {
+  let result = ''; 
+    for(let i = 0; i < s1.length; i++){ 
+      if(s1[i] === '+' && s2[i] === '+') result += '+'
+      else if(s1[i] === '-' && s2[i] === '-') result += '-'
+      else result += 0
+    }
+  return result
+}
