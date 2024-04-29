3b074405a7ecbc4ab0920318ad39150891f1111a
Kkatsiarryna <katepicastor@gmail.com>
2024-04-29 23:44:41 +0300

    Make Me Slow

---
+++ 7kyu/make-me-slow.js
@@ -0,0 +1,9 @@
+// DESCRIPTION
+//Make me slow! Calling makeMeSlow() should take at least 7 seconds.
+
+//SOLUTION
+function makeMeSlow() {
+    const start = Date.now();
+    while ( Date.now() < start + 7000){
+    }
+}
